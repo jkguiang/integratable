c2100b6554d865e764dd179c1b1c12bb3ada580d
Jonathan Guiang <jkguiang@gmail.com>
2018-10-17 07:33:47 -0700

    added integral cards

--- src/Integrals.js
+++ src/Integrals.js
@@ -1,23 +1,52 @@
 import React, { Component } from 'react';
-import { Container, Jumbotron } from 'reactstrap';
+import { Container,
+         Jumbotron,
+         Button,
+         Card, CardHeader, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 import './App.css';
+import 'katex/dist/katex.min.css';
+import { InlineMath, BlockMath } from 'react-katex';
+
+var test = "\\int_a^b x^n dx = \\frac{1}{n+1}x^{n+1}, x \\neq -1";
+var testtest = "n"
+
+class IntegralCard extends Component {
+    render() {
+        var contStyle = {
+            paddingBottom: "15px",
+            paddingLeft: "0",
+            paddingRight: "0"
+        }
+        return (
+            <Container style={contStyle}>
+              <div className="card">
+                <CardHeader className="text-center"><BlockMath math={this.props.integral} /></CardHeader>
+                <div className="card-body">
+                  <CardTitle>Evaluation Parameters</CardTitle>
+                  <Button outline color="success" block>Submit</Button>
+                </div>
+              </div>
+            </Container>
+        );
+    }
+}
 
 class Integrals extends Component {
-  render() {
-    return (
-        <Container>
-          <Jumbotron className="p-3 p-md-5 text-white rounded bg-dark">
-            <div className="col-md-6 px-0">
-              <h1 className="display-4 font-italic">Common Integrals</h1>
-              <p className="lead my-3">Quick reference table with basic integrals one is likely to see often.</p>
-            </div>
-          </Jumbotron>
-          <div className="text-center">
-              <p className="text-muted"><i>Integratable is interactive! To get a definite solution, fill in the user input form in the right column and hit enter.</i></p>
-          </div>
-        </Container>
-    );
-  }
+    render() {
+        return (
+            <Container>
+              <Jumbotron className="p-3 p-md-5 text-white rounded bg-dark">
+                <div className="col-md-6 px-0">
+                  <h1 className="display-4">Basic Integrals</h1>
+                  <p className="lead my-3 font-italic">Quick reference table with integrals one is likely to see often.</p>
+                </div>
+              </Jumbotron>
+              <IntegralCard integral={test}/>
+              <IntegralCard integral={test}/>
+              <IntegralCard integral={test}/>
+            </Container>
+        );
+    }
 }
 
 export default Integrals;
