a172154e546209d8d0c95fb47ab71224fcd44f7d
Jonathan Guiang <jkguiang@gmail.com>
2018-11-03 18:39:16 -0700

    added two new integrals

--- src/IntegralDB.js
+++ src/IntegralDB.js
@@ -9,6 +9,16 @@ const IntegralDB = {
             "integral": "\\int_a^b \\frac{1}{x} dx = \\ln(x)",
             "constants": ["a","b"],
             "query": ["integrate ", "1/x", " from ", "a", " to ", "b"]
+        },
+        {
+            "integral": "\\int_a^b \\frac{1}{cx+d} dx = \\frac{1}{c}\\ln(x)\\biggr\\rvert cx+d \\biggr\\rvert",
+            "constants": ["a","b","c","d"],
+            "query": ["integrate ", "1/(", "c", "x", "+", "d", ")", " from ", "a", " to ", "b"]
+        },
+        {
+            "integral": "\\int_a^b (x+c)^n dx = \\frac{(x+c)^{n+1}}{n+1} \\textnormal{, } n \\neq -1",
+            "constants": ["a","b","c", "n"],
+            "query": ["integrate ", "(", "x", "+", "c", ")", "^", "n", " from ", "a", " to ", "b"]
         }
     ]
 
