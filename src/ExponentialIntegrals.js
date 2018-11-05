d1d6fcb0c5324cdb06a2635b20a6503e0e609445
Jonathan Guiang <jkguiang@gmail.com>
2018-11-04 16:25:34 -0800

    corrected queries

--- src/ExponentialIntegrals.js
+++ src/ExponentialIntegrals.js
@@ -7,12 +7,12 @@ const ExponentialDB = [
     {
         "integral": "\\int_a^b e^{cx} dx = \\frac{1}{c}e^{cx}",
         "constants": ["a","b","c"],
-        "query": ["integrate ","x","^","n"," from ","a"," to ","b"]
+        "query": ["integrate ","e","^","(","c","x",")"," from ","a"," to ","b"]
     },
     {
         "integral": "\\int_a^b x^{n}e^{cx} dx = \\frac{x^{n}e^{cx}}{c}-\\frac{n}{c}\\int_a^b x^{n-1}e^{cx} dx",
         "constants": ["a","b","c","n"],
-        "query": ["integrate ","1","/","x"," from ","a"," to ","b"]
+        "query": ["integrate ","x","^","n","e","^","(","c","x",")"," from ","a"," to ","b"]
     }
 ];
 
@@ -23,7 +23,7 @@ class ExponentialIntegrals extends Component {
                 <Jumbotron className="p-3 p-md-5 text-white rounded bg-dark">
                   <div className="col-md-6 px-0">
                     <h1 className="display-4">Exponential Integrals</h1>
-                    <p className="lead my-3 font-italic">Quick reference table with integrals one is likely to see often.</p>
+                    <p className="lead my-3 font-italic">Integrals involving exponential functions.</p>
                   </div>
                 </Jumbotron>
                 <Integrals db={ExponentialDB} />
