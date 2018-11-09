ab0e6f57c9c799004810c8036bdc5fe9eefd0304
Jonathan Guiang <jkguiang@gmail.com>
2018-11-09 10:46:18 -0800

    updated integratable blurbs

--- src/About.js
+++ src/About.js
@@ -17,17 +17,19 @@ class About extends Component {
                   <h2><FontAwesomeIcon icon="info-circle" /> What is this?</h2>
                   <hr/>
                   <p>
-                  Integratable is an interactive integral table. As a service, we have two goals. First, we aim to provide useful integrals on an interactive, modern platform. There are many static, PDF integral tables out there, but Integratable uses React to make integrals easier to find and more pleasing to look at. We also utilize the Wolfram Alpha API to make every integral on this website evaluatable as a definite integral. Second, we are also a non-profit organization hoping to channel support from interested individuals to worthy causes. More info on this is provided below.
+                  Integratable is an interactive integral table. We aim to provide useful integrals on an interactive, modern platform. There are many static, PDF integral tables out there, but Integratable uses React to make integrals easier to find and more pleasing to look at. We also utilize the Wolfram Alpha API to make every integral on this website evaluatable as a definite integral.
                   </p>
+                  <p><br /></p>
                   <h2><FontAwesomeIcon icon="calculator" /> How do I use it?</h2>
                   <hr/>
                   <p>
-                  The <a href="https://www.integratable.info">Home</a> page can be used like any ordinary integral table. However, you may also click any integral to be navigated to its interactive counterpart. There, you will be able to enter values for any variables and have the now definite integral solved by Wolfram Alpha for you!
+                  The <a href="https://www.integratable.info"><b>Home</b></a> page can be used like any ordinary integral table. However, you may also click any integral to be navigated to its interactive counterpart. There, you will be able to enter values for any variables and have the now definite integral solved by Wolfram Alpha for you!
                   </p>
+                  <p><br /></p>
                   <h2><FontAwesomeIcon icon="heart" /> How can I help?</h2>
                   <hr/>
                   <p>
-                  As mentioned at the top, Integratable is a non-profit organization. We are always looking for charitable individuals to help us make Integratable a better service. However, moving this website to more scalable backend technologies is not very expensive. Thus, should we garner enough support, we plan to direct the generosity of our patrons to worthy causes with a focus on furthering the extent and equity of education. For more information, please visit our <a href="https://www.patreon.com/integratable">Patreon page</a>. All of the names of our patrons can be found on the <a href="https://www.integratable.info/#/donate">Donate</a> page.
+                  We are always looking for charitable individuals to help us make Integratable a better service. However, moving this website to more scalable backend technologies is not very expensive, so we plan to direct any excess generosity of our patrons to worthy causes with a focus on furthering the extent and equity of education. For more information, please visit our <a href="https://www.patreon.com/integratable"><b>Patreon page</b></a>. All of the names of our patrons can be found on the <a href="https://www.integratable.info/#/donate"><b>Donate</b></a> page.
                   </p>
                 </Container>
               </React.Fragment>
