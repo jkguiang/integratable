5cc911108423f147b77a5fabf79a1b6be2aed692
Jonathan Guiang <jkguiang@gmail.com>
2018-11-09 10:45:38 -0800

    added new exponential integrals

--- src/ExponentialIntegrals.js
+++ src/ExponentialIntegrals.js
@@ -10,10 +10,25 @@ const ExponentialDB = [
         "query": ["integrate ","e","^","(","c","x",")"," from ","a"," to ","b"]
     },
     {
+        "integral": "\\int_a^b xe^{cx} dx = (\\frac{x}{a}-\\frac{1}{a^2})e^{cx}",
+        "constants": ["a","b","c"],
+        "query": ["integrate ","x","e","^","(","c","x",")"," from ","a"," to ","b"]
+    },
+    {
+        "integral": "\\int_a^b x^{2}e^{cx} dx = (\\frac{x^2}{a}-\\frac{2x}{a^2}+\\frac{2}{a^3})e^{cx}",
+        "constants": ["a","b","c"],
+        "query": ["integrate ","x","e","^","(","c","x",")"," from ","a"," to ","b"]
+    },
+    {
         "integral": "\\int_a^b x^{n}e^{cx} dx = \\frac{x^{n}e^{cx}}{c}-\\frac{n}{c}\\int_a^b x^{n-1}e^{cx} dx",
         "constants": ["a","b","c","n"],
         "query": ["integrate ","x","^","n","e","^","(","c","x",")"," from ","a"," to ","b"]
-    }
+    },
+    {
+        "integral": "\\int_{-\\infty}^{\\infty} e^{-A(x+B)^2} dx = \\sqrt{\\frac{\\pi}{A}}",
+        "constants": ["A","B"],
+        "query": ["integrate ","e","^","(","A","*","-","(","x","+","B",")","^","2",")"," from ","-","infinity"," to ","infinity"]
+    },
 ];
 
 class ExponentialIntegrals extends Component {
