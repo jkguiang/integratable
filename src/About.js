cad525c43340f1a68ee59d910d81202ef1ac8027
Jonathan Guiang <jkguiang@gmail.com>
2018-11-14 15:20:58 -0800

    updated donation/about pages

--- src/About.js
+++ src/About.js
@@ -17,19 +17,25 @@ class About extends Component {
                   <h2><FontAwesomeIcon icon="info-circle" /> What is this?</h2>
                   <hr/>
                   <p>
-                  Integratable is an interactive integral table. We aim to provide useful integrals on an interactive, modern platform. There are many static, PDF integral tables out there, but Integratable uses React to make integrals easier to find and more pleasing to look at. We also utilize the Wolfram Alpha API to make every integral on this website evaluatable as a definite integral.
+                  Integratable is an interactive integral table. There are many static, PDF integral tables out there, but Integratable uses React to make integrals easier to find and more pleasing to look at. All of the integrals on this website are already solved, so any integral can be easily evaluated without any fancy numerical approximations or other operations usually handled by advanced backend software.
                   </p>
+                  <h4 className="text-center">
+                    <a href="https://reactjs.org/"><FontAwesomeIcon icon={['fab', 'react']}/></a>&nbsp;
+                    <a href="https://nodejs.org/en/"><FontAwesomeIcon icon={['fab', 'node-js']}/></a>&nbsp;
+                    <a href="https://github.com/"><FontAwesomeIcon icon={['fab', 'github']}/></a>&nbsp;
+                    <a href="https://fontawesome.com/"><FontAwesomeIcon icon={['fab', 'font-awesome-flag']}/></a>
+                  </h4>
                   <p><br /></p>
                   <h2><FontAwesomeIcon icon="calculator" /> How do I use it?</h2>
                   <hr/>
                   <p>
-                  The <a href="https://www.integratable.info"><b>Home</b></a> page can be used like any ordinary integral table. However, you may also click any integral to be navigated to its interactive counterpart. There, you will be able to enter values for any variables and have the now definite integral solved by Wolfram Alpha for you!
+                  The <a href="https://www.integratable.info"><b>Home</b></a> page can be used like any ordinary integral table. However, you may also click any integral to be navigated to its interactive counterpart. There, you may simply enter the proper values for each constant and the answer will be computed for you!
                   </p>
                   <p><br /></p>
                   <h2><FontAwesomeIcon icon="heart" /> How can I help?</h2>
                   <hr/>
                   <p>
-                  We are always looking for charitable individuals to help us make Integratable a better service. However, moving this website to more scalable backend technologies is not very expensive, so we plan to direct any excess generosity of our patrons to worthy causes with a focus on furthering the extent and equity of education. For more information, please visit our <a href="https://www.patreon.com/integratable"><b>Patreon page</b></a>. All of the names of our patrons can be found on the <a href="https://www.integratable.info/#/donate"><b>Donate</b></a> page.
+                  Integratable is entirely free to host, since the website is entirely static. However, if you really enjoy the page, or just feel exceptionally generous, you can buy me a coffee on my <a href="https://ko-fi.com/jguiang#"><b>Ko-fi</b></a> page. All of the names of those who have funded my caffeine addiction can be found on the <a href="https://www.integratable.info/#/donate"><b>Donate</b></a> page. Thanks for using Integratable!
                   </p>
                 </Container>
               </React.Fragment>
--- src/Donate.js
+++ src/Donate.js
@@ -1,49 +1,48 @@
 import React, { Component } from 'react';
-import { Jumbotron, Container, Button } from 'reactstrap';
+import { Jumbotron, Container } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './App.css';
 
 class Donate extends Component {
       render() {
           var btnStyle = {
-              color: "#fff"
+              border:"0",
+              height:"36px",
           };
           return (
               <React.Fragment>
                 <Jumbotron className="p-3 p-md-5 text-white rounded bg-dark">
                   <div className="col-md-6 px-0">
                     <h1 className="display-4">Better Integrals for Everyone</h1>
-                    <p className="lead my-3 font-italic">Consider saving the world by direct integration!</p>
-                    <Button color="success" href="https://www.patreon.com/integratable" style={btnStyle}>
-                      <FontAwesomeIcon icon={['fab', 'patreon']}/> Patreon
-                    </Button>
+                    <p className="lead my-3 font-italic">Thanks for using the website!</p>
+                    <a href='https://ko-fi.com/N4N6M2CZ' target="_blank" rel="noopener noreferrer"><img height='36' style={btnStyle} src='https://az743702.vo.msecnd.net/cdn/kofi3.png?v=0' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
                   </div>
                 </Jumbotron>
                 <Container>
-                  <h2><FontAwesomeIcon icon={['fab', 'patreon']}/> Our Patrons</h2>
+                  <h2><FontAwesomeIcon icon="coffee" /> Supporters</h2>
                   <hr/>
                   <p>
-                  To have your name listed here, consider becoming a patron! Some funds go towards the monthly upkeep for the site, but any overflow support will be directed to worthy causes. See our <a href="https://www.patreon.com/integratable">Patreon page</a> for more info.
+                  To have your name listed here, buy me a coffee! Know, however, that this website is entirely free to host, so please feel no obligation to send money my way. Thanks for using Integratable!
                   </p>
-                  <h5><FontAwesomeIcon icon="grin" /> Integral Fans</h5>
-                  <p className="text-muted"><i>"Appreciates integrals and integral websites."</i></p>
-                  <ul>
-                    <li><i>For $1 a month, you can become the first Integral Fan!</i></li>
-                  </ul>
-                  <h5><FontAwesomeIcon icon="grin-hearts" /> Integral Fanatics</h5>
-                  <p className="text-muted"><i>"Worrisome love of integrals, wants to see Integratable take over the world."</i></p>
-                  <ul>
-                    <li><i>For $5 a month, you can become the first Integral Fanatic!</i></li>
-                  </ul>
-                  <h5><FontAwesomeIcon icon="hands" /> Integral Devotees</h5>
-                  <p className="text-muted"><i>"Unflinching devotion to the reversal of derivatives. Holds the Fundamental Theorem of Calculus close to their heart."</i></p>
-                  <ul>
-                    <li><i>For $10 a month, you can become the first Integral Devotee!</i></li>
-                  </ul>
                   <h5><FontAwesomeIcon icon="street-view" /> Integral Deities</h5>
-                  <p className="text-muted"><i>"Non-etherial being that contributed to the inscription of the Fundamental Theorem of Calculus unto the mortal plane. Operates as the infinite sum of individual, differential consciousnesses."</i></p>
+                  <p className="text-muted"><i>"Non-etherial beings that contributed to the inscription of the Fundamental Theorem of Calculus unto the mortal plane. Operate as the infinite sum of individual, differential consciousnesses."</i></p>
                   <ul>
-                    <li><i>For $20 a month, you can become the first Integral Deity!</i></li>
+                    <li><i>For one coffee, you can become the first Integral Deity!</i></li>
+                  </ul>
+                  <h5><FontAwesomeIcon icon="hands" /> Integral Paladin</h5>
+                  <p className="text-muted"><i>"Holy knights bound to the corporeal realm. Provided at birth with nth-dimensional clairvoyance such that the indefinite integrals of all functions are made clear. Fiercely committed to the Path of the Integral."</i></p>
+                  <ul>
+                    <li><i>For one coffee, you can become the first Integral Prophet!</i></li>
+                  </ul>
+                  <h5><FontAwesomeIcon icon="dragon" /> Antiderivative Daemon</h5>
+                  <p className="text-muted"><i>"Non-euclidian entities tasked only with the ceaseless reversal of all derivatives. Dispenses the secrets of the Antiderivative for a price..."</i></p>
+                  <ul>
+                    <li><i>For one coffee, you can become the first Antiderivative Daemon!</i></li>
+                  </ul>
+                  <h5><FontAwesomeIcon icon="fist-raised" /> Antiderivative Champion</h5>
+                  <p className="text-muted"><i>"Warrior of Direct Integration imbued witg an ability of numerical integration unrivaled by any mortal. Knows only the glory of battle in the endless struggle against derivation."</i></p>
+                  <ul>
+                    <li><i>For one coffee, you can become the first Antiderivative Champion!</i></li>
                   </ul>
                 </Container>
               </React.Fragment>
