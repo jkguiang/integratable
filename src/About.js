911f8b8de495ea41bc0a1359be79e457670ccc30
Jonathan Guiang <jkguiang@gmail.com>
2022-09-02 08:54:40 -0700

    moved to https://jkguiang.github.io/integratable

--- src/About.js
+++ src/About.js
@@ -43,7 +43,7 @@ class About extends Component {
               <h2><FontAwesomeIcon icon="calculator" /> How do I use it?</h2>
               <hr/>
               <p>
-              The <a href="https://www.integratable.info"><b>Home</b></a> page can be used like any ordinary integral table. However, you may also click any integral to be navigated to its interactive counterpart like the one displayed below. To evaluate the integral, simply enter the desired values for each constant in their corresponding boxes and hit submit. Additionally, you can anchor (<FontAwesomeIcon icon="anchor"/>) the integral to the URL (this highlights the integral on your screen and automatically scrolls anyone else that loads that URL to the same integral), copy (<FontAwesomeIcon icon="clipboard"/>) its LaTeX to your clipboard, or easily clear (<FontAwesomeIcon icon="undo-alt"/>) the input form. Try it out!
+              The <a href="https://jkguiang.github.io/integratable"><b>Home</b></a> page can be used like any ordinary integral table. However, you may also click any integral to be navigated to its interactive counterpart like the one displayed below. To evaluate the integral, simply enter the desired values for each constant in their corresponding boxes and hit submit. Additionally, you can anchor (<FontAwesomeIcon icon="anchor"/>) the integral to the URL (this highlights the integral on your screen and automatically scrolls anyone else that loads that URL to the same integral), copy (<FontAwesomeIcon icon="clipboard"/>) its LaTeX to your clipboard, or easily clear (<FontAwesomeIcon icon="undo-alt"/>) the input form. Try it out!
               </p>
               <Container>
                 <IntegralCard integral={demo.integral} restrict={(demo.hasOwnProperty("restrict")) ? demo.restrict : ""} constants={demo.constants} query={demo.query} plot={demo.plot} key={0} index={0} />
@@ -51,7 +51,7 @@ class About extends Component {
               <h2><FontAwesomeIcon icon="heart" /> How can I help?</h2>
               <hr/>
               <p>
-              Integratable is entirely free to host, since the website is entirely static. However, if you really enjoy the page, or just feel exceptionally generous, you can buy me a coffee on my <a href="https://ko-fi.com/jguiang#" target="_blank" rel="noopener noreferrer"><b>Ko-fi</b></a> page. All of the names of those who have funded my caffeine addiction can be found on the <a href="https://www.integratable.info/#/donate"><b>Donate</b></a> page. You can also help by suggesting additional integrals <a href="https://github.com/jkguiang/integratable/issues" target="_blank" rel="noopener noreferrer"><b>here</b></a>, provided you have a Github account and a knowledge or desire to learn LaTeX.
+              Integratable is entirely free to host, since the website is entirely static. However, if you really enjoy the page, or just feel exceptionally generous, you can buy me a coffee on my <a href="https://ko-fi.com/jguiang#" target="_blank" rel="noopener noreferrer"><b>Ko-fi</b></a> page. All of the names of those who have funded my caffeine addiction can be found on the <a href="https://jkguiang.github.io/integratable"><b>Donate</b></a> page. You can also help by suggesting additional integrals <a href="https://github.com/jkguiang/integratable/issues" target="_blank" rel="noopener noreferrer"><b>here</b></a>, provided you have a Github account and a knowledge or desire to learn LaTeX.
               </p>
             </Container>
           </React.Fragment>
